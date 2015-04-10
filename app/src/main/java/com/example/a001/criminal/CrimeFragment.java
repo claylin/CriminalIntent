8e32a20e04ae7d473011e352c02213ced937ac86
claylin <claylin.007@gmail.com>
2015-04-10 10:23:32 +0800

    Ancestral Navigation enabled

--- app/src/main/java/com/example/a001/criminal/CrimeFragment.java
+++ app/src/main/java/com/example/a001/criminal/CrimeFragment.java
@@ -1,13 +1,20 @@
 package com.example.a001.criminal;
 
+import android.annotation.TargetApi;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -21,9 +28,6 @@ import java.util.UUID;
 
 /**
  * A fragment representing a single Item detail screen.
- * This fragment is either contained in a {@link CrimeActivity}
- * in two-pane mode (on tablets)
- * on handsets.
  */
 public class CrimeFragment extends Fragment {
     public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
@@ -53,14 +57,41 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (NavUtils.getParentActivityName(getActivity()) != null) {
+                    NavUtils.navigateUpFromSameTask(getActivity());
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @TargetApi(11)
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+//            if (NavUtils.getParentActivityName(getActivity()) != null) {
+//                ((ActionBarActivity)getActivity()).getActionBar().setDisplayHomeAsUpEnabled(true);
+//            }
+            ActionBarActivity a = (ActionBarActivity)getActivity();
+            if (a != null) {
+                ActionBar b = a.getSupportActionBar();
+                if (b != null)
+                    b.setDisplayHomeAsUpEnabled(true);
+            }
+        }
 
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
         mTitleField.setText(mCrime.getTitle());
