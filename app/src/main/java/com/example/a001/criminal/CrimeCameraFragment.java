23973607782ac90bd1e63c0135cfb2f95928b010
claylin <claylin.007@gmail.com>
2015-04-12 13:30:19 +0800

    Take picture works, page 347.

--- app/src/main/java/com/example/a001/criminal/CrimeCameraFragment.java
+++ app/src/main/java/com/example/a001/criminal/CrimeCameraFragment.java
@@ -1,5 +1,6 @@
 package com.example.a001.criminal;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.view.SurfaceView;
 import android.view.LayoutInflater;
@@ -14,23 +15,68 @@ import android.annotation.TargetApi;
 import android.util.Log;
 import android.hardware.Camera.Size;
 
+import java.io.FileOutputStream;
 import java.util.List;
 import java.io.IOException;
+import java.util.UUID;
 
 public class CrimeCameraFragment extends Fragment {
     private static final String TAG = "CrimeCameraFragment";
 
     private Camera mCamera;
     private SurfaceView mSurfaceView;
+    private View mProgressContainer;
+
+    private Camera.ShutterCallback mShutterCallback = new Camera.ShutterCallback() {
+        public void onShutter() {
+            // Display the progress indicator
+            mProgressContainer.setVisibility(View.VISIBLE);
+        }
+    };
+
+    private Camera.PictureCallback mJpegCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // Create a filename
+            String filename = UUID.randomUUID().toString() + ".jpg";
+            // Save the jpeg data to disk
+            FileOutputStream os = null;
+            boolean success = true;
+            try {
+                os = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
+                os.write(data);
+            } catch (Exception e) {
+                Log.e(TAG, "Error writing to file " + filename, e);
+                success = false;
+            } finally {
+                try {
+                    if (os != null)
+                        os.close();
+                } catch (Exception e) {
+                    Log.e(TAG, "Error closing file " + filename, e);
+                    success = false;
+                }
+            }
+            if (success) {
+                Log.i(TAG, "JPEG saved at " + filename);
+            }
+            getActivity().finish();
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime_camera, parent, false);
+
+        mProgressContainer = v.findViewById(R.id.crime_camera_progressContainer);
+        mProgressContainer.setVisibility(View.INVISIBLE);
+
         Button takePictureButton = (Button)v.findViewById(R.id.crime_camera_takePictureButton);
 
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                getActivity().finish();
+                if (mCamera != null) {
+                    mCamera.takePicture(mShutterCallback, null, mJpegCallback);
+                }
             }
 		});
 
@@ -66,6 +112,8 @@ public class CrimeCameraFragment extends Fragment {
 	            Camera.Parameters parameters = mCamera.getParameters();
 	            Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), w, h);
 	            parameters.setPreviewSize(s.width, s.height);
+                s = getBestSupportedSize(parameters.getSupportedPictureSizes(), w, h);
+                parameters.setPictureSize(s.width, s.height);
 	            mCamera.setParameters(parameters);
 	            try {
 	                mCamera.startPreview();
